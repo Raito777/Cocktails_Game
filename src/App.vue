1dcd03f60251d07884f91ab94f0d6666f12bf4be
Quentin AUGEY <quentin.augey@gmail.com>
2023-02-14 18:03:51 +0100

    testing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>
+      Galery
+    </h1>
+  </div>
+  <GalleryDiv msg="1er test"></GalleryDiv>
+  <div class="message">
+    <h2>Ceci est un message</h2>
+    <div class="quoteDiv">
+      <QuoteDiv msg="quote"></QuoteDiv>
+      <QuoteDiv msg="quote"></QuoteDiv>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GalleryDiv from './components/GalleryDiv';
+import QuoteDiv from './components/QuoteDiv';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GalleryDiv,
+    QuoteDiv
   }
 }
 </script>
@@ -21,6 +34,24 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin:0;
+  padding:0;
+}
+html {
+  margin:0;
+  padding:0;
+}
+body{
+  margin:0;
+  padding:0;
+}
+.message{
+  background-color:rgb(255, 196, 196);
+  padding:50px;
+}
+.quoteDiv{
+  display:flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 </style>
---
+++ src/components/GalleryDiv.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <CardDiv></CardDiv>
+        <CardDiv></CardDiv>
+        <CardDiv></CardDiv>
+        <CardDiv></CardDiv>
+    </div>
+</template>
+
+<script>
+    import CardDiv from './CardDiv.vue';
+    export default {
+    name: 'GalleryDiv',
+    components: {
+    CardDiv
+    },
+    props: {
+        msg: String
+    }
+    }
+</script>
+
+<style scoped>
+    div{
+        display:flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        background-color: antiquewhite;
+        border-color: 1px solid black;
+        padding:25px;
+    }
+</style>
